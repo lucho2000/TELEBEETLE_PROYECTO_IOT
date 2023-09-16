bf7662689bd06c72db1a1302ef9a39b030a21a7e
r.b4rr10s <rodrigoedu11@gmail.com>
2023-09-15 22:29:06 -0500

    Cambios aesthetic a login y register

--- app/src/main/java/com/example/telebeetle/MainActivity.java
+++ app/src/main/java/com/example/telebeetle/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.telebeetle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.telebeetle.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
     }
 }
