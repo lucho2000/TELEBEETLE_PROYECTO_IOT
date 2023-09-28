eb63709a7332c5c24d8ce049354ad1e6e61bb685
xTarantino28 <gzanabriaz28@gmail.com>
2023-09-27 23:24:41 -0500

    zoom automatico del mapa para tener ubicacion actual. valores decentes entre 16 y 18

--- app/src/main/java/com/example/telebeetle/MapsActivity.java
+++ app/src/main/java/com/example/telebeetle/MapsActivity.java
@@ -58,6 +58,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng position = new LatLng(latitud, longitud);
         mMap.addMarker(new MarkerOptions().position(position));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(16));
         // Add a marker in Sydney and move the camera
         //LatLng sydney = new LatLng(-34, 151);
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
