8ee11d9e27662fe960639657c82fb7f5c8199570
r.b4rr10s <rodrigoedu11@gmail.com>
2023-09-27 22:49:25 -0500

    Primer avance google maps, ubicar marcador con ubicacion actual en el mapa

--- app/src/main/java/com/example/telebeetle/DetallesEvento1.java
+++ app/src/main/java/com/example/telebeetle/DetallesEvento1.java
@@ -1,14 +1,23 @@
 package com.example.telebeetle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.example.telebeetle.databinding.ActivityDetallesEvento1Binding;
 import com.example.telebeetle.databinding.ActivityMainBinding;
 import com.example.telebeetle.fragments.OpcionesApoyar;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 public class DetallesEvento1 extends AppCompatActivity {
     ActivityDetallesEvento1Binding binding;
@@ -30,9 +39,57 @@ public class DetallesEvento1 extends AppCompatActivity {
                     add(R.id.fragmentContainerView, OpcionesApoyar.class, null).commit();
         });
 
+        binding.goMapa.setOnClickListener(view -> {
+            mostrarUbicacion();
+        });
+
     }
     public void deleteFragment(Fragment fragmentToDelete) {
         // Use FragmentManager to remove the specified fragment
         getSupportFragmentManager().beginTransaction().remove(fragmentToDelete).commit();
     }
+    public void mostrarUbicacion() {
+
+        int selfPermissionFineLocation = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+        int selfPermissionCoarseLocation = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if (selfPermissionFineLocation == PackageManager.PERMISSION_GRANTED &&
+                selfPermissionCoarseLocation == PackageManager.PERMISSION_GRANTED) {
+            //tenemos permisos
+            FusedLocationProviderClient providerClient = LocationServices.getFusedLocationProviderClient(this);
+            providerClient.getLastLocation().addOnSuccessListener(this, location -> {
+                if(location != null){
+                    Log.d("msg-test","latitud: " + location.getLatitude());
+                    Log.d("msg-test","longitud: " + location.getLongitude());
+                    Intent intent = new Intent(DetallesEvento1.this, MapsActivity.class);
+                    intent.putExtra("latitud", location.getLatitude());
+                    intent.putExtra("longitud", location.getLongitude());
+                    startActivity(intent);
+                }
+            });
+        } else {
+            //no tenemos permisos, se deben solicitar
+            locationPermissionLauncher.launch(new String[]{
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+            });
+        }
+
+    }
+    ActivityResultLauncher<String[]> locationPermissionLauncher = registerForActivityResult(
+            new ActivityResultContracts.RequestMultiplePermissions(),
+            result -> {
+                Boolean fineLocationGranted = result.get(android.Manifest.permission.ACCESS_FINE_LOCATION);
+                Boolean coarseLocationGranted = result.get(android.Manifest.permission.ACCESS_COARSE_LOCATION);
+                if (fineLocationGranted != null && fineLocationGranted) {
+                    Log.d("msg", "Permiso de ubicación precisa concedido");
+                    mostrarUbicacion();
+                } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                    Log.d("msg", "Permiso de ubicación aproximada concedido");
+                } else {
+                    Log.d("msg", "Ningún permiso concedido");
+                }
+            }
+    );
+
 }
---
+++ app/src/main/java/com/example/telebeetle/MapsActivity.java
@@ -0,0 +1,76 @@
+package com.example.telebeetle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.telebeetle.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    private Double latitud = -33.865143;
+    private Double longitud = 151.209900;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intent = getIntent();
+        latitud = intent.getDoubleExtra("latitud", 1.0);
+        longitud = intent.getDoubleExtra("longitud", 1.0);
+        Log.d("msg-test","latitud en maps: " + latitud);
+        Log.d("msg-test","longitud en maps: " + longitud);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        this.mMap.setOnMapClickListener(this);
+        this.mMap.setOnMapLongClickListener(this);
+        LatLng position = new LatLng(latitud, longitud);
+        mMap.addMarker(new MarkerOptions().position(position));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+        // Add a marker in Sydney and move the camera
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onMapClick(@NonNull LatLng latLng) {
+
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+
+    }
+}
