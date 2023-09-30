cbb5e9cf6a20f62616c75c6554b3677fbd648fa4
CarlosPisco <edupiscote.cept@gmail.com>
2023-09-30 02:54:05 -0500

    donacionesss

--- app/src/main/java/com/example/telebeetle/fragments/DonationFragment.java
+++ app/src/main/java/com/example/telebeetle/fragments/DonationFragment.java
@@ -1,15 +1,26 @@
 package com.example.telebeetle.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.telebeetle.Entity.Donacion;
+import com.example.telebeetle.Entity.Evento;
 import com.example.telebeetle.R;
+import com.example.telebeetle.activities.DonacionesAdapter;
+import com.example.telebeetle.activities.EventAdapter;
+import com.example.telebeetle.activities.QRDonarActivity;
 import com.example.telebeetle.databinding.FragmentDonationBinding;
+
+import java.lang.annotation.Inherited;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DonationFragment extends Fragment {
@@ -21,8 +32,47 @@ public class DonationFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentDonationBinding.inflate(inflater,container,false);
 
+        cargarlistaDonaciones();
+
+
+
+        binding.buttonDonar.setOnClickListener(view -> {
+
+            Intent intent = new Intent(container.getContext(), QRDonarActivity.class);
+            startActivity(intent);
+
+        });
 
 
         return binding.getRoot();
     }
+
+
+    public List<Donacion> listarDonacionesHardcoded (){
+
+        List<Donacion> listDona  = new ArrayList<>();
+
+        Donacion dona1 = new Donacion("Donacion para la AITEL","10 de Octubre, 2023","15:00");
+        Donacion dona2 = new Donacion("Donacion para la AITEL","11 de Octubre, 2023","14:00");
+        Donacion dona3 = new Donacion("Donacion para la AITEL","12 de Octubre, 2023","16:00");
+        Donacion dona4 = new Donacion("Donacion para la AITEL","13 de Octubre, 2023","18:00");
+
+        listDona.add(dona1);
+        listDona.add(dona2);
+        listDona.add(dona3);
+        listDona.add(dona4);
+
+        return listDona;
+
+    }
+
+    public void cargarlistaDonaciones(){
+        List<Donacion> listaDonaciones = listarDonacionesHardcoded();
+        DonacionesAdapter donacionesAdapter = new DonacionesAdapter();
+        donacionesAdapter.setListDonaciones(listaDonaciones);
+        donacionesAdapter.setContext(getActivity().getApplicationContext());
+        binding.rvDonaciones.setAdapter(donacionesAdapter);
+        binding.rvDonaciones.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
+    }
+
 }
