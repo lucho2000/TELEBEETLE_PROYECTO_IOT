6a14336e96b42a7021cb8ace58d2a57939f6d55f
unknown <luis.ramost@pucp.edu.pe>
2023-10-15 17:08:44 -0500

    codigo para autenticacion con google, falta obtener el SHA que pide la pagina

--- app/src/main/java/com/example/telebeetle/activities/MainActivity.java
+++ app/src/main/java/com/example/telebeetle/activities/MainActivity.java
@@ -1,21 +1,82 @@
 package com.example.telebeetle.activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.telebeetle.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    FirebaseAuth.AuthStateListener firebaseAuthListener;
+
+    BeginSignInRequest signInRequest;
+
+    GoogleApi googleApiClient;
+
+    final int REQ_ONE_TAP = 2;  // Can be any integer unique to the Activity.
+    boolean showOneTapUI = true;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //codigo para autenticacion con google
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        //mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+        signInRequest = BeginSignInRequest.builder()
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(true)
+                        .build())
+                .build();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+
+                }
+            }
+        };
+
+
 
         /* LINKEO HACIA REGISTRO DE USUARIO */
         TextView registerTextView = findViewById(R.id.registrarmeTextView);
@@ -51,5 +112,59 @@ public class MainActivity extends AppCompatActivity {
         });
         /* LINKEO HACIA ACTIVITY GENERAL VIEW*/
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        firebaseAuth.addAuthStateListener(firebaseAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (firebaseAuthListener != null) {
+            firebaseAuth.removeAuthStateListener(firebaseAuthListener);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_ONE_TAP:
+                try {
+                    SignInClient oneTapClient = null;
+                    SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(data);
+                    String idToken = credential.getGoogleIdToken();
+                    if (idToken != null) {
+
+                        // Got an ID token from Google. Use it to authenticate
+                        // with Firebase.
+                        AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
+                        firebaseAuth.signInWithCredential(firebaseCredential)
+                                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            // Sign in success, update UI with the signed-in user's information
+                                            //Log.d(TAG, "signInWithCredential:success");
+                                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                                            //updateUI(user);
+                                        } else {
+                                            // If sign in fails, display a message to the user.
+                                            //Log.w(TAG, "signInWithCredential:failure", task.getException());
+                                            //updateUI(null);
+                                        }
+                                    }
+                                });
+                    }
+                } catch (ApiException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+        }
     }
 }
