1117b85730fd2fed0dfc64e14cee22a28084fb4c
xTarantino28 <gzanabriaz28@gmail.com>
2023-10-20 22:14:37 -0500

    funciona editar evento xd

--- app/src/main/java/com/example/telebeetle/activities/EditarEventoActivity.java
+++ app/src/main/java/com/example/telebeetle/activities/EditarEventoActivity.java
@@ -1,28 +1,46 @@
 package com.example.telebeetle.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.telebeetle.Entity.Evento;
 import com.example.telebeetle.databinding.ActivityEditarEventoBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Objects;
+import java.util.TimeZone;
 
 
 public class EditarEventoActivity extends AppCompatActivity {
 
     TextInputLayout textInputLayout;
-    EditText editTextDatePicker;
+    TextInputEditText editTextDatePicker;
     ActivityEditarEventoBinding binding;
 
+    FirebaseDatabase database;
+    Evento eventoObjEditar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,16 +48,35 @@ public class EditarEventoActivity extends AppCompatActivity {
         binding = ActivityEditarEventoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        //obtener intent con id del evento.
+        String idEvento = "1";
 
 
+        editTextDatePicker = binding.editTextDate;
 
+
+        database = FirebaseDatabase.getInstance();
+        rellenarConDataCamposEditarEvento(idEvento);
 
         textInputLayout = binding.FechaTextField;
-        editTextDatePicker = binding.editTextDate;
         textInputLayout.setEndIconOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showMaterialDesignDatePicker();
+            }
+        });
+
+        binding.botonEditarEvento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editarDataEventoFirebase(idEvento);
+            }
+        });
+
+        binding.botonCancelarEditarEvento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
 
@@ -54,11 +91,75 @@ public class EditarEventoActivity extends AppCompatActivity {
         materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
             @Override
             public void onPositiveButtonClick(Long selection) {
-                String date = new SimpleDateFormat("dd-MM-yyy", Locale.getDefault()).format(new Date(selection));
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+                dateFormat.setTimeZone(TimeZone.getTimeZone("UTC")); //importante para cuando seleccionas una fecha, no devuelva la fecha anterior durante el formateo
+
+                String date = dateFormat.format(new Date(selection));
+                //String date = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).setTimeZone(TimeZone.getDefault()).format(new Date(selection));
                 editTextDatePicker.setText(MessageFormat.format("{0}",date));
             }
         });
         materialDatePicker.show(getSupportFragmentManager(),"tag");
+    }
+
+    public void rellenarConDataCamposEditarEvento(String idEvento) {
+        TextInputEditText miActividadEditarEvento = binding.nombreActividadEvento;
+        TextInputEditText nombreEvento = binding.nombreEvento;
+        EditText descripcionEvento = binding.editTextDescripcion;
+        DatabaseReference eventosData = database.getReference("evento");
+        eventosData.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
+                    if (Objects.requireNonNull(dataSnapshot.getKey()).equalsIgnoreCase(idEvento)) {
+                        eventoObjEditar = dataSnapshot.getValue(Evento.class);
+                        if (eventoObjEditar != null) {
+                            Log.d("msg-test-firebase",eventoObjEditar.getFecha());
+                            editTextDatePicker.setText(eventoObjEditar.getFecha());
+                            miActividadEditarEvento.setText(eventoObjEditar.getActividad());
+                            nombreEvento.setText(eventoObjEditar.getEtapa()); //revisar atributos objeto evento
+                            //descripcionEvento.setText(eventoObjEditar.getDescripcion());
+                            descripcionEvento.setText("no hay descripcion aun como campo en firebase");
+                        } else {
+                            //objeto nulo
+                            Log.d("msg-test-firebase","objeto evento nulo. no match with ID");
+                        }
+                    }
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("msg-test-firebase","Ocurrio un error al obtener evento especifico (con id) en firebase. EditarEventoActivity");
+            }
+        });
+    }
+
+    public void editarDataEventoFirebase(String idEvento) {
+        String miActividadEditarEvento = binding.nombreActividadEvento.getText().toString();
+        String nombreEvento = binding.nombreEvento.getText().toString();
+        String descripcionEvento = binding.editTextDescripcion.getText().toString();
+        DatabaseReference eventosData = database.getReference("evento");
+        HashMap<String, Object> eventoUpdate = new HashMap<>();
+        eventoUpdate.put("actividad",miActividadEditarEvento);
+        eventoUpdate.put("etapa",nombreEvento);
+        eventoUpdate.put("fecha",editTextDatePicker.getText().toString());
+        //eventoUpdate.put("lugar",);
+
+        eventosData.child(idEvento).updateChildren(eventoUpdate).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    rellenarConDataCamposEditarEvento(idEvento);
+                    Toast.makeText(EditarEventoActivity.this,"Success to update event",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(EditarEventoActivity.this,"Failed to update event",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
     /*
