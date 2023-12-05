ba776a543fdfe9c56f478b7c08c84b27f9739b1d
lucho2000 <luis.ramost@pucp.edu.pe>
2023-12-04 22:45:53 -0500

    banear usuarios, falta arreglar boton y borrarlo del auth de firebase y probar con sesion

--- app/src/main/java/com/example/telebeetle/activities/UsuarioAdapter.java
+++ app/src/main/java/com/example/telebeetle/activities/UsuarioAdapter.java
@@ -1,17 +1,24 @@
 package com.example.telebeetle.activities;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.telebeetle.Entity.Usuario;
 import com.example.telebeetle.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -21,11 +28,41 @@ public class UsuarioAdapter extends RecyclerView.Adapter<UsuarioAdapter.UsuarioV
     private List<Usuario> listaUsuarios;
     private Context context;
 
+
+    DatabaseReference databaseReference;
+    DatabaseReference databaseReference2;
+
     public class UsuarioViewHolder extends RecyclerView.ViewHolder{
 
         Usuario user;
         public UsuarioViewHolder(@NonNull View itemView) {
             super(itemView);
+            Button banear = itemView.findViewById(R.id.buttonBanear);
+            databaseReference = FirebaseDatabase.getInstance().getReference("usuarios_por_admitir");
+            banear.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    databaseReference2 = FirebaseDatabase.getInstance().getReference("usuarios");
+                    user.setEnable(false); //si se cambia, ya no deberia entrar, pero se deberia borrar del auth tambien
+                    databaseReference2.child(user.getUidUsuario()).setValue(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Log.d("msg-test","enable del usuario: "+ user.getEnable());
+                            //user.setEnable(false); //si se cambia, ya no deberia entrar, pero se deberia borrar del auth tambien
+
+                            Toast.makeText(context, "Usuario borrado de la aplicacion", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(context, "No se pudo borrar al usuario", Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+                }
+            });
+
         }
     }
 
